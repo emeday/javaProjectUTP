7e8fb3a21d88b74015b582e5a29abf738640bc0f
Emeday <1312124@utp.edu.pe>
2024-04-11 21:27:32 -0500

    Subida de proyecto en Java Medidas de Tendencia Central - inicio

---
+++ MedidasTenCen/src/main/java/com/proyutp/medidastencen/MedidasTenCen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyutp.medidastencen;
+
+/**
+ *
+ * @author emeda
+ */
+public class MedidasTenCen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
